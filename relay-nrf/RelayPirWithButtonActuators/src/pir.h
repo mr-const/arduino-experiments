c6ea35e034acd55fbb7440bc7a9207f58aabad9b
Konst Kolesnichenko <kolesnichenko@gmail.com>
2016-07-31 20:53:52 +0300

    * added some documentation

--- relay-nrf/RelayPirWithButtonActuators/src/pir.h
+++ relay-nrf/RelayPirWithButtonActuators/src/pir.h
@@ -16,9 +16,21 @@ private:
 	static void onPirEvent();
 
 public:
+	//************************************
+	// Method:    init: initializes PIR module singleton.
+	// Parameter: uint8_t signalPin: pin with attached PIR signal line. Must be one of the pins with an interrupt support (2,3 for arduino pro mini).
+	//************************************
 	static void init(uint8_t signalPin);
+	//************************************
+	// Method:    loop: must be called from main loop, executes callback for PIR motion event.
+	//************************************
 	static void loop();
 
+	//************************************
+	// Method:    setPirEventHandler: installs PIR event handler, or clears if NULL/nullptr.
+	// Parameter: PirEventHandler handler, which called on main loop after hardware interrupt received, 
+	// has one parameter: tripped - true when tripped, false - when back to armed (idle) state.
+	//************************************
 	static void setPirEventHandler(PirEventHandler handler);
 };
 
