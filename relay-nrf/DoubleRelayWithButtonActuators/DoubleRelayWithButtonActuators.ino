9b29d817f8485344e938051083b979b6d5281fe1
Konst Kolesnichenko <kolesnichenko@gmail.com>
2016-08-01 23:05:14 +0300

    * updated to MySensors 2.0 library * added msvs project

--- relay-nrf/DoubleRelayWithButtonActuators/DoubleRelayWithButtonActuators.ino
+++ relay-nrf/DoubleRelayWithButtonActuators/DoubleRelayWithButtonActuators.ino
@@ -3,7 +3,8 @@
 // pin 3 and GND).
 // This node also works as a repeader for other nodes
 
-#include <MySensor.h>
+#define MY_RADIO_NRF24
+#include <MySensors.h>
 #include <SPI.h>
 #include <Bounce2.h>
 
@@ -16,112 +17,110 @@
 #define RELAY_ON 1
 #define RELAY_OFF 0
 
-Bounce debouncer = Bounce(); 
-int oldValue=0;
+Bounce debouncer = Bounce();
+int oldValue = 0;
 bool state;
-Bounce debouncer2 = Bounce(); 
-int oldValue2=0;
+Bounce debouncer2 = Bounce();
+int oldValue2 = 0;
 bool state2;
 
-MySensor gw;
-MyMessage msg(CHILD_ID,V_LIGHT);
-MyMessage msg2(CHILD_ID_2,V_LIGHT);
+MyMessage msg(CHILD_ID, V_LIGHT);
+MyMessage msg2(CHILD_ID_2, V_LIGHT);
 
-void setup()  
-{  
-  gw.begin(incomingMessage, AUTO, true);
+void presentation() {
+	// Send the sketch version information to the gateway and Controller
+	sendSketchInfo("Double Relay & Button", "0.2");
+	// Register all sensors to gw (they will be created as child devices)
+	present(CHILD_ID, S_LIGHT);
+	present(CHILD_ID_2, S_LIGHT);
+}
 
-  // Send the sketch version information to the gateway and Controller
-  gw.sendSketchInfo("Double Relay & Button", "0.1");
+void setup()
+{
+	// Setup the button
+	pinMode(BUTTON_PIN, INPUT);
+	// Activate internal pull-up
+	digitalWrite(BUTTON_PIN, HIGH);
 
-  // Setup the button
-  pinMode(BUTTON_PIN,INPUT);
-  // Activate internal pull-up
-  digitalWrite(BUTTON_PIN,HIGH);
-  
-   // Setup the button
-  pinMode(BUTTON_PIN_2,INPUT);
-  // Activate internal pull-up
-  digitalWrite(BUTTON_PIN_2,HIGH);
-  
-  // After setting up the button, setup debouncer
-  debouncer.attach(BUTTON_PIN);
-  debouncer.interval(5);
-  
-  debouncer2.attach(BUTTON_PIN_2);
-  debouncer2.interval(5);
+	// Setup the button
+	pinMode(BUTTON_PIN_2, INPUT);
+	// Activate internal pull-up
+	digitalWrite(BUTTON_PIN_2, HIGH);
 
-  // Register all sensors to gw (they will be created as child devices)
-  gw.present(CHILD_ID, S_LIGHT);
-  gw.present(CHILD_ID_2, S_LIGHT);
+	// After setting up the button, setup debouncer
+	debouncer.attach(BUTTON_PIN);
+	debouncer.interval(5);
 
-  // Make sure relays are off when starting up
-  digitalWrite(RELAY_PIN, RELAY_OFF);
-  // Then set relay pins in output mode
-  pinMode(RELAY_PIN, OUTPUT);   
-  
-  digitalWrite(RELAY_PIN_2, RELAY_OFF);
-  // Then set relay pins in output mode
-  pinMode(RELAY_PIN_2, OUTPUT);   
-      
-  // Set relay to last known state (using eeprom storage) 
-  state = gw.loadState(CHILD_ID);
-  digitalWrite(RELAY_PIN, state?RELAY_ON:RELAY_OFF);
-  
-  state2 = gw.loadState(CHILD_ID_2);
-  digitalWrite(RELAY_PIN_2, state2?RELAY_ON:RELAY_OFF);
-  
+	debouncer2.attach(BUTTON_PIN_2);
+	debouncer2.interval(5);
+
+
+	// Make sure relays are off when starting up
+	digitalWrite(RELAY_PIN, RELAY_OFF);
+	// Then set relay pins in output mode
+	pinMode(RELAY_PIN, OUTPUT);
+
+	digitalWrite(RELAY_PIN_2, RELAY_OFF);
+	// Then set relay pins in output mode
+	pinMode(RELAY_PIN_2, OUTPUT);
+
+	// Set relay to last known state (using eeprom storage) 
+	state = loadState(CHILD_ID);
+	digitalWrite(RELAY_PIN, state ? RELAY_ON : RELAY_OFF);
+
+	state2 = loadState(CHILD_ID_2);
+	digitalWrite(RELAY_PIN_2, state2 ? RELAY_ON : RELAY_OFF);
+
 }
 
 
 /*
 *  Example on how to asynchronously check for new messages from gw
 */
-void loop() 
+void loop()
 {
-  gw.process();
-  debouncer.update();
-  debouncer2.update();
-  // Get the update value
-  int value = debouncer.read();
-  int value2 = debouncer2.read();
-  
-  if (value != oldValue) {
-      gw.send(msg.set(state?false:true), true); // Send new state and request ack back
-  }
-  oldValue = value;
-  
-  if (value2 != oldValue2) {
-      gw.send(msg2.set(state2?false:true), true); // Send new state and request ack back
-  }
-  oldValue2 = value2;  
-} 
- 
-void incomingMessage(const MyMessage &message) {
-  // We only expect one type of message from controller. But we better check anyway.
-  if (message.isAck()) {
-     Serial.println("This is an ack from gateway");
-  }
+	debouncer.update();
+	debouncer2.update();
+	// Get the update value
+	int value = debouncer.read();
+	int value2 = debouncer2.read();
 
-  if (message.type == V_LIGHT && message.sensor == 11) {
-     // Change relay state
-     state = message.getBool();
-     digitalWrite(RELAY_PIN, state?RELAY_ON:RELAY_OFF);
-     // Store state in eeprom
-     gw.saveState(CHILD_ID, state);
-   }
-  else if(message.type == V_LIGHT && message.sensor == 12){  
-    
-     state2 = message.getBool();
-     digitalWrite(RELAY_PIN_2, state2?RELAY_ON:RELAY_OFF);
-     // Store state in eeprom
-     gw.saveState(CHILD_ID_2, state2);
-    
-    
-     // Write some debug info
-     Serial.print("Incoming change for sensor:");
-     Serial.print(message.sensor);
-     Serial.print(", New status: ");
-     Serial.println(message.getBool());
-   } 
+	if (value != oldValue) {
+		send(msg.set(state ? false : true), true); // Send new state and request ack back
+	}
+	oldValue = value;
+
+	if (value2 != oldValue2) {
+		send(msg2.set(state2 ? false : true), true); // Send new state and request ack back
+	}
+	oldValue2 = value2;
+}
+
+void receive(const MyMessage &message) {
+	// We only expect one type of message from controller. But we better check anyway.
+	if (message.isAck()) {
+		Serial.println("This is an ack from gateway");
+	}
+
+	if (message.type == V_LIGHT && message.sensor == 11) {
+		// Change relay state
+		state = message.getBool();
+		digitalWrite(RELAY_PIN, state ? RELAY_ON : RELAY_OFF);
+		// Store state in eeprom
+		saveState(CHILD_ID, state);
+	}
+	else if (message.type == V_LIGHT && message.sensor == 12) {
+
+		state2 = message.getBool();
+		digitalWrite(RELAY_PIN_2, state2 ? RELAY_ON : RELAY_OFF);
+		// Store state in eeprom
+		saveState(CHILD_ID_2, state2);
+
+
+		// Write some debug info
+		Serial.print("Incoming change for sensor:");
+		Serial.print(message.sensor);
+		Serial.print(", New status: ");
+		Serial.println(message.getBool());
+	}
 }
